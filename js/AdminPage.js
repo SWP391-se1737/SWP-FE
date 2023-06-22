3e8f8abfe11956954e9671d0884aa95402edefcd
nhattanAugust1st <nhattan08012000@gmail.com>
2023-06-22 08:27:43 +0700

    api call

--- js/AdminPage.js
+++ js/AdminPage.js
@@ -17,3 +17,59 @@ toggle.onclick = function(){
     navigation.classList.toggle("active");
     main.classList.toggle("active");
 };
+
+async function getProductList() {
+    try {
+        const res = await fetch(':localhost:8080/product/getListProduct');
+        const data = await res.json();
+        if(!res.ok){
+            console.log(data.description);
+            return;
+        }
+        let tableData="";
+        data.map(products =>{
+            tableData+= `<tr>
+            <td>${products.id}<td>
+            <td>${products.name}<td>
+            <td>${products.image}<td>
+            <td>${products.description}<td>
+            <td>${products.price}<td>
+            <td>${products.createat}<td>
+            <td>${products.exprire}<td>
+            <td>${products.status}<td>
+            <td>${products.quantity}<td>
+            </tr>`;
+        });
+        document.getElementById("table_body").innerHTML=tableData;
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+async function getAccountList() {
+    try {
+        const data = await fetch('http://localhost:8080/Account/listAccount');
+        const objectData = await data.json();
+        if(!res.ok){
+            console.log(objectData.description);
+            return;
+        }
+        let tableData="";
+        objectData.map(accounts =>{
+            tableData+= `<tr>
+            <td>${accounts.id}<td>
+            <td>${accounts.email}<td>
+            <td>${accounts.phone}<td>
+            <td>${accounts.role}<td>
+            <td>${accounts.status}<td>
+            </tr>`;
+        });
+        document.getElementById("table_body").innerHTML=tableData;
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
