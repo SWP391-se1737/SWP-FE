023236544271d731dc450a35b3bfb84ded1d6349
nhattanAugust1st <nhattan08012000@gmail.com>
2023-07-06 07:40:06 +0700

    add func add new category

--- js/ManageCategory.js
+++ js/ManageCategory.js
@@ -1,17 +1,40 @@
 async function getCategoryList() {
     const response = await axios.get('http://localhost:8080/Category/listCategory');
       categories = response.data;
-      console.log(categories);
       let tableData="";
       categories.map(categories =>{
-                    tableData+= `<tr>
-                      <td>${categories.id}</td>
-                      <td>${categories.name}</td>
-                      <td>${categories.status}</td>
-                      <td><button>Update</button></td>
-                      <td><button>Delete</button></td>
-                    </tr>`;
-                });
-                document.getElementById("table_body").innerHTML=tableData;
-    }
-    getCategoryList();
+                tableData+= `<tr>
+                <td id="categoryID">${categories.id}</td>
+                <td id="categoryName">${categories.name}</td>
+                <td id="categoryStatus">${categories.status}</td>
+                <td><a href="#" class="update">Update</a>
+                <a a href="#" class="delete">Delete</a>
+                                    </tr>`;
+                                });
+                                document.getElementById("category-list").innerHTML=tableData;
+}
+getCategoryList();
+
+document.querySelector("#category-form").addEventListener("click", (e)=>{
+  target = e.target;
+  if(target.classList.contains("add")){
+    const addCategoryForm = document.getElementById("category-form");
+    const catergoryName = addCategoryForm.elements.catergoryName.value;
+    axios.post('http://localhost:8080/Category/addCategory', {
+      name : catergoryName,
+      status : 'true'
+  })
+  }
+});
+
+document.querySelector("#category-list").addEventListener("click", (e)=>{
+  target = e.target;
+  if(target.classList.contains("delete")){
+    const categoryName = document.getElementById("categoryName");
+    console.log(categoryName);
+    // const categoryName = categoryName.elements.categoryName.value;
+    axios.delete('http://localhost:8080/Category/deleteCategory', {
+    name : categoryName
+  })
+  }
+});
--- js/ManageStudent.js
+++ js/ManageStudent.js
@@ -5,17 +5,39 @@ async function getStudentList() {
     let tableData = "";
     students.map(students => {
         if (students.role == ("Student")) {
+            // tableData += `<tr>
+            // <td><input type="text" name="email" value="${students.email}" readonly=""/></td>
+            // <td><input type="text" name="id" value="${students.id}" readonly=""/></td>
+            // <td><input type="text" name="name" value="${students.name}" readonly=""/></td>
+            // <td><input type="text" name="phone" value="${students.phone}" readonly=""/></td>
+            // <td><input type="text" name="role" value="${students.role}" readonly=""/></td>
+            // <td><input type="text" name="status" value="${students.status}" readonly=""/></td>
+            // <td><a a href="#" class="delete">Delete</a></td>
+            // </tr>`;
             tableData += `<tr>
-            <td>${students.email}</td>
+            <td id="studentEmail" value="${students.email}">${students.email}</td>
             <td>${students.id}</td>
             <td>${students.name}</td>
             <td>${students.phone}</td>
             <td>${students.role}</td>
             <td>${students.status}</td>
-            <td><button>Delete</button></td>
+            <td><a a href="#" class="delete">Delete</a></td>
             </tr>`;
         }
     });
-    document.getElementById("table_body").innerHTML = tableData;
+    document.getElementById("student-list").innerHTML = tableData;
 }
 getStudentList();
+
+document.querySelector("#student-list").addEventListener("click", (e)=>{
+    target = e.target;
+    if(target.classList.contains("delete")){
+      const studentEmail = document.getElementById("studentEmail");
+      console.log(studentEmail);
+      const studentEmail1 = studentEmail.elements.studentEmail.value;
+      console.log(studentEmail1);
+      axios.delete('http://localhost:8080/Account/deleteAccount/', {
+      email : studentEmail
+    })
+    }
+  });
