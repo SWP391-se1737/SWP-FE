285cb20bd96fffd1b92f6bb21b30695c3f6f2f71
khanhncd2008 <rinright1306@gmail.com>
2023-07-20 23:07:32 +0700

    multi-img

--- js/Category_Campus.js
+++ js/Category_Campus.js
@@ -71,7 +71,10 @@ const renderProductsByCampusAndCategory = (products) => {
             divItem.dataset.key = productId;
 
             const img = document.createElement('img');
-            img.src = result.image;
+            const imageUrls = result.image.split(",");
+            if (imageUrls.length > 0) {
+                img.src = imageUrls[0];
+            }
 
             const h2 = document.createElement('h5');
             h2.innerText = result.name;
