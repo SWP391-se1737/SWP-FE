33095241a34c0dcffcbf62c20ed8628bb86eee22
nhattanAugust1st <nhattan08012000@gmail.com>
2023-07-20 11:06:32 +0700

    load data for chart in adminpage

--- js/AdminPage.js
+++ js/AdminPage.js
@@ -1,54 +1,79 @@
-async function getProductList() {
+async function getTotalProducts() {
     const response = await axios.get('http://localhost:8080/product/getListProduct');
     products = response.data;
-    for (const product of products) {
-
+    var categoryID = await getcategoryID();
+    var totalProducts = [];
+    var i = 0;
+    for (i = 0; i < categoryID.length; i++) {   // Gán giá trị 0 vào tất cả vị trí trong array
+         totalProducts[i] = 0;
     }
 
+    for (const product of products) {
+        for (i = 0; i < categoryID.length; i++) {
+            if(product.categoryid === categoryID[i]){
+                totalProducts[i] += 1;
+            }
+       }
+    }
+    return totalProducts;
 }
 
-async function getCategoryList() {
+async function getCategoryName() {
     const response = await axios.get('http://localhost:8080/Category/listCategory');
     categories = response.data;
-    console.log(categories);
     var categoryName = [];
+    var i = 0;
     for (const category of categories) {
-        var i = 0;
         categoryName[i] = category.name;
         i++;
     };
-    console.log(categoryName);
+    return categoryName;
 }
 
+async function getcategoryID() {
+    const response = await axios.get('http://localhost:8080/Category/listCategory');
+    categories = response.data;
+    var categoryID = [];
+    var i = 0;
+    for (const category of categories) {
+        categoryID[i] = category.id;
+        i++;
+    };
+    return categoryID;
+}
 
-var categories = ['Quần Áo', 'Thiết bị điện tử', 'Giày', 'Cặp sách', 'Khác', 'Nhạc Cụ'];
-var times = ['6', '6', '3', '3', '2', '1'];
-getCategoryList();
+async function renderChart() {
+    var categoryName = await getCategoryName();
+    
+    var totalProducts = await getTotalProducts();
 
-const options = {
-    labels: categories,
-    series: times,
-    chart: {
-        type: 'polarArea',
-    },
-    stroke: {
-        colors: ['#fff']
-    },
-    fill: {
-        opacity: 0.8
-    },
-    responsive: [{
-        breakpoint: 480,
-        options: {
-            chart: {
-                width: 200
-            },
-            legend: {
-                position: 'bottom'
+    const options = {
+        labels: categoryName,
+        series: totalProducts,
+        chart: {
+            type: 'polarArea',
+        },
+        stroke: {
+            colors: ['#fff']
+        },
+        fill: {
+            opacity: 0.8
+        },
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
             }
-        }
-    }]
-};
+        }]
+    };
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
-chart.render();
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+}
+
+renderChart();
